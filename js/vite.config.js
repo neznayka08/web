8f22c468dcd7e7e5d26515c6e7925539bd54845f
neznayka08 <123936929+neznayka08@users.noreply.github.com>
2025-03-25 12:52:16 +0700

    lab_14

---
+++ js/vite.config.js
@@ -0,0 +1,9 @@
+export default {
+    server: {
+      host: "0.0.0.0",
+      allowedHosts: [
+        "6cca57c4-596c-4113-827d-852080ed6638-00-2d1va9o0otg9c.sisko.replit.dev",
+      ],
+    },
+  };
+  
